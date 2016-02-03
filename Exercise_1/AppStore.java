ae5731cfd5231f8b3fec1e9e2397b9fc5325d700
Parlin Nando IF3801 <parlinnando98@gmail.com>
2016-02-03 07:58:26 +0700

    Update AppStore.java

--- Exercise_1/AppStore.java
+++ Exercise_1/AppStore.java
@@ -1,1 +1,27 @@
-
+/* [IF-38-01] _ PArlin Nando _ 1103132235 */
+public class AppStore{
+	private Application[] appList = new Application[100];
+	private int totalApp = 0;
+	
+	public void createNewApp(String appName, int appSize){
+		Application a = new Application(appName, appSize);
+		appList[totalApp] = a;
+		totalApp++;
+	}
+	
+	public Application getApp(int id){
+		return appList[id];
+	}
+	
+	public int getTotalApp() {
+		return totalApp;
+	}
+	
+	public Application[] getAppList(){
+		return appList;
+	}
+	
+	public String toString(){
+		return ("There are "+totalApp+" applications ready to install.");
+	}
+}	
