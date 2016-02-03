f139e935b8d91a18af7f0e64f4a07c716be48f17
Parlin Nando IF3801 <parlinnando98@gmail.com>
2016-02-03 08:07:56 +0700

    Update Project.java

--- Exercise_2/Project.java
+++ Exercise_2/Project.java
@@ -1,1 +1,37 @@
-
+/* [IF-38-01] _ Parlin Nando _ 1103132235 */
+public class Project {
+	private String projectName;
+	private Member[] teamMember;
+	private int nTeam = 0;
+	private boolean releaseStatus;
+	
+	public Project (String projectName) {
+		this.projectName = projectName;
+		teamMember = new Member[5];
+		releaseStatus = false;
+	}
+	
+	public void addMember(Member m) {
+		teamMember[nTeam] = m;
+		nTeam++;
+	}
+	
+	public boolean isReleased () {
+	return releaseStatus;
+	}
+	
+	public void releaseApp() {
+		releaseStatus = true;
+	}
+	
+	public String toString() {
+        String stat;
+        if(releaseStatus = true) {
+            stat = "Released";
+        }
+        else {
+            stat = "in Progress";
+        }
+		return ("Project "+projectName+" status is "+stat+" with team member of "+nTeam);
+	}
+}
