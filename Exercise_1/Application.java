439b068d750d6f24f3eee333e7143a946bece841
Parlin Nando IF3801 <parlinnando98@gmail.com>
2016-02-03 08:00:53 +0700

    Update Application.java

--- Exercise_1/Application.java
+++ Exercise_1/Application.java
@@ -1,1 +1,24 @@
+/* [IF-38-01] _ Parlin Nando _ 1103132235 */
+public class Application{
+	private String appName;
+	private int appSize;
+	
+	public void Application(String appName, int appSize){
+		this.appName = appName;
+		this.appSize = appSize;
+	}
+	
+	public String getappName() {
+		return appName;
+	}
+	
+	public int getappSize() {
+		return appSize;
+	}
+	
+	public String toString(){
+		return ("Application name: "+appName+", Size: "+appSize);
+	}
+	
+}
 
